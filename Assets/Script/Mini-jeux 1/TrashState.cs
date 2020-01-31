234d3647cadacafdfbd2b1a325d8514eb84b95c5
SauzeauYannis <sauzeau-yannis@gmail.com>
2020-01-31 23:52:45 +0100

    Add TrashState.cs

---
+++ Assets/Script/Mini-jeux 1/TrashState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashState : MonoBehaviour
+{
+    int state = 3; //States number initialized to 3
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterTheTrash(GameObject gameObject)
+    {
+        //If state is 0 then destroy the object
+        if (state == 0)
+        {
+            Destroy(gameObject);
+        }
+        //else state is descrement
+        else state--;
+    }
+}
