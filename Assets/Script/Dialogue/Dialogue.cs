3433748a460e5b51018e99a034a86e415c30c44f
linkouille <linkouillou@gmail.com>
2020-01-31 22:20:01 +0100

    Initial Commit to pull

---
+++ Assets/Script/Dialogue/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu()]
+public class Dialogue : ScriptableObject
+{
+    public string text;
+    public float speed;
+    public bool choose;
+
+    public Dialogue nextOption_1;
+    public Dialogue nextOption_2;
+
+}
