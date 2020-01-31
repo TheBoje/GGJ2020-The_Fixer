4127c825a60a7bc2130197772f725ed28cbd5476
linkouille <linkouillou@gmail.com>
2020-01-31 20:54:45 +0100

    PLayer Move
    Player base and Scene Setup

---
+++ Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 input;
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        rb.velocity = input * speed;
+    }
+
+}
