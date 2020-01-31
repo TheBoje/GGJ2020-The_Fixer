138b69aeb81e922821548a599773dc663a4a4aa8
linkouille <linkouillou@gmail.com>
2020-02-01 00:06:08 +0100

    Revert "Dialogue Systheme"
    This reverts commit ef4273e332b0fc0da22348e1a1fb94f706d7ac3b.

--- Assets/Script/Dialogue/Dialogue.cs
+++ Assets/Script/Dialogue/Dialogue.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "New Dialogue")]
+[CreateAssetMenu()]
 public class Dialogue : ScriptableObject
 {
-    [TextArea]public string text;
-    [Range(0,1)]public float speed;
+    public string text;
+    public float speed;
     public bool choose;
 
     public Dialogue nextOption_1;
-    public string textChoix1;
     public Dialogue nextOption_2;
-    public string textChoix2;
 
 }
