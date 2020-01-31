719bb46ef029800ffd179beef39ac945c5438e1d
TheBoje <louis.leenar@gmail.com>
2020-02-01 00:01:24 +0100

    PlayerTrash.cs added

---
+++ Assets/Script/Player/PlayerTrash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrash : MonoBehaviour
+{
+    [SerializeField] private GameObject Trash;
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Trash")
+        {
+            Trash.GetComponent<TrashState>.EnterTheTrash(collision.gameObject);
+        }
+    }
+}
