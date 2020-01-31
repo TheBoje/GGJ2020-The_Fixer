76d07712ca5779301341d64428a37e9af97ddd01
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-01-31 23:23:35 +0100

    ajout du pont

---
+++ Assets/Script/MiniGame-Bridge/Bridge.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+
+
+    [SerializeField] private const short _nbRepairMax = 3;  // Nombre de réparation à effectuer
+    [SerializeField] private short _nbRepair;               // Nombre de réparations encore nécessaire
+    [SerializeField] private bool _state;                   // Etat du pont (false = détruit, true = reconstruit)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _nbRepair = _nbRepairMax;
+        _state = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            foreach (Transform child in collision.transform)
+            {
+
+            }
+        }
+    }
+
+    public short nbRepair
+    {
+        get { return _nbRepair; }
+    }
+
+    public bool state
+    {
+        get { return _state; }
+    }
+}
---
+++ Assets/Script/MiniGame-Bridge/BridgeParts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeParts : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("YO");
+
+        if (collision.tag == "Player")
+        {
+            //transform.position = player.transform.position;
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            this.transform.parent = player.transform;
+        }
+    }
+}
