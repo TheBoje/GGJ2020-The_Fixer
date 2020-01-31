6de97ef03571b5b87f4b9745092a2618e1f61d1a
SauzeauYannis <sauzeau-yannis@gmail.com>
2020-02-01 00:01:28 +0100

    modify TrashState.cs

--- Assets/Script/Mini-jeux 1/TrashState.cs
+++ Assets/Script/Mini-jeux 1/TrashState.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class TrashState : MonoBehaviour
 {
-    int state = 3; //States number initialized to 3
+    [SerializeField] private int state; //States number initialized to 3
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = 3;
     }
 
     // Update is called once per frame
