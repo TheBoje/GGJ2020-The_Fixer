4319a2683c5609cf40890577f1594851ab693bb8
SauzeauYannis <sauzeau-yannis@gmail.com>
2020-02-01 02:30:24 +0100

    Modify TrashState.cs

--- Assets/Script/Mini-jeux 1/TrashState.cs
+++ Assets/Script/Mini-jeux 1/TrashState.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class TrashState : MonoBehaviour
 {
-    [SerializeField] public int state; //Variable etat
-    // Start is called before the first frame update
-    void Start()
-    {
-        state = 3; //Initialisation de l'etat a 3
-    }
+    [SerializeField] private int minTime; // Variable du temps minimal
+    [SerializeField] private int maxTime; // Variable du temps maximal
+    [SerializeField] private int minNum; // Variable du nombre minimal
+    [SerializeField] private int maxNum; // Variable du nombre maximal
+    private int[,] array;
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
-    }
+        minTime = 1;
+        maxTime = 3;
+        minNum = 5;
+        maxNum = 10;
+        array = new int[Random.Range(minNum, maxNum + 1), 2];
 
-    public void EnterTheTrash(GameObject gameObject)
-    {
-        //Si l'etat est a zero alors l'objet est detruit
-        if (state == 0)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            Destroy(gameObject);
+            int random = Random.Range(1, 4);
+
+            array[i, 0] = random;
         }
-        //Sinon on decremente l'etat
-        else state--;
+
+        for (int i = 0; i < array.GetLength(0) - 1; i++)
+        {
+            int random = Random.Range(minTime, maxTime + 1);
+
+            array[i, 1] = random;
+        }
+
+        array[array.GetLength(0), 1] = 0;
+
+        gameObject.GetComponent<GameScriptMG1>().StartQTE(this.gameObject, array);
     }
 }
