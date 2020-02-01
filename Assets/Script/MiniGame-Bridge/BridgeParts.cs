a24d0a2db9a0d6e5fdbaeffc3e31aa95ad8da49c
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-02-01 19:32:55 +0100

    le player ne peut prendre qu'un morceau de vase à chaque fois

--- Assets/Script/MiniGame-Bridge/BridgeParts.cs
+++ Assets/Script/MiniGame-Bridge/BridgeParts.cs
@@ -28,10 +28,18 @@ public class BridgeParts : MonoBehaviour
 
         if (collision.tag == "Player")
         {
-            //transform.position = player.transform.position; // l'objet vient se mettre sur le personnage
-            this.GetComponent<SpriteRenderer>().enabled = false; // On désactive son rendu
-            this.GetComponent<BoxCollider2D>().enabled = false; // On désactive son collider
-            this.transform.parent = playerInventory.transform; // On l'envois en tant que fils de inventory
+
+            bool isFull = false;
+
+            foreach (Transform item in playerInventory.transform)
+                isFull = true;
+            if(!isFull)
+            {
+                //transform.position = player.transform.position; // l'objet vient se mettre sur le personnage
+                this.GetComponent<SpriteRenderer>().enabled = false; // On désactive son rendu
+                this.GetComponent<BoxCollider2D>().enabled = false; // On désactive son collider
+                this.transform.parent = playerInventory.transform; // On l'envois en tant que fils de inventory
+            }
         }
     }
 
