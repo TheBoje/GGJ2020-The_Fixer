be1a3b6f2de06cbb8c6c2b70ec9fcd8dc93f7f73
SauzeauYannis <sauzeau-yannis@gmail.com>
2020-02-01 20:21:02 +0100

    Add of Obstacle.prefab + ObstacleScript.cs + Poulet.prefab + Poulet.prefab

---
+++ Assets/Script/Mini-jeux Poulet/ChickenScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            transform.position = player.transform.position;      // l'objet vient se mettre sur le personnage
+            this.GetComponent<SpriteRenderer>().enabled = false; // On désactive son rendu
+            this.GetComponent<BoxCollider2D>().enabled = false;  // On désactive son collider
+        }
+    }
+}
---
+++ Assets/Script/Mini-jeux Poulet/ObstacleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject chicken;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 playerCoor = player.transform.position;
+            Vector3 moov = new Vector3(playerCoor.x + 10.0f, 0.0f, 0.0f);
+
+            chicken.GetComponent<SpriteRenderer>().enabled = true;
+            chicken.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
