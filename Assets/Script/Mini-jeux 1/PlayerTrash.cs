c8f3f0a1e9887945f5494f97e2dc9e79eb4092e2
TheBoje <louis.leenar@gmail.com>
2020-02-01 01:45:51 +0100

    intégration interact

--- Assets/Script/Mini-jeux 1/PlayerTrash.cs
+++ Assets/Script/Mini-jeux 1/PlayerTrash.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class PlayerTrash : MonoBehaviour
 {
-    [SerializeField] private GameObject Trash;
-    void Update()
-    {
+    public bool isInteracting = false;  // Booléen de vérification : permet de ne pas lancer la boucle de minijeu pour netoyer ea
 
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Trash")
+        if (collision.gameObject.tag == "Trash" && Input.GetButtonDown("Use") && isInteracting == false)
         {
-            Trash.GetComponent<TrashState>().EnterTheTrash(collision.gameObject);
-            //Debug.Log(collision.gameObject.GetComponent<TrashState>().State);
+            //collision.gameObject.GetComponent<TrashState>().Interact();
+            isInteracting = true;
+        }
+        else if ( isInteracting == true)
+        {
+            Debug.Log("Le personnage est déjà en train d'intéragir. Le bool est mis à false quelque part?");
         }
     }
 }
