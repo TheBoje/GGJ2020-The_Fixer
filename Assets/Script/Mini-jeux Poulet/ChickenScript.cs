48dd0dde5043151bd7bc5f56c3ace763709f3de5
TheBoje <louis.leenar@gmail.com>
2020-02-02 02:50:44 +0100

    WORKING Jeu poulet et QTE 100%

--- Assets/Script/Mini-jeux Poulet/ChickenScript.cs
+++ Assets/Script/Mini-jeux Poulet/ChickenScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class ChickenScript : MonoBehaviour
 {
-    [SerializeField] private GameObject player;  // Prend le joueur
+    [SerializeField] private GameObject player;  
+    [SerializeField] public bool isFollowingPlayer = false;
+    [SerializeField] private int distMin = 2;
+    [SerializeField] private float speed = 1.5f;
+
+    private Vector3 Direction;
+    private float Distance;
 
     public void OnTriggerStay2D(Collider2D collision)   // Quand on reste dans un trigger
     {
-        if (collision.tag == "Player")  // Si le joueur collisionne avec l'objet
+        if (collision.tag == "Player" && !isFollowingPlayer)  // Si le joueur collisionne avec l'objet
         {
-            transform.position = player.transform.position;      // l'objet vient se mettre sur le personnage
-            this.GetComponent<SpriteRenderer>().enabled = false; // On désactive son rendu
-            this.GetComponent<BoxCollider2D>().enabled = false;  // On désactive son collider
+            isFollowingPlayer = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isFollowingPlayer)
+        {
+            Distance = Vector3.Distance(player.transform.position, transform.position);
+            if (Distance > distMin)
+            {
+                transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * speed);
+            }
         }
     }
 }
--- Assets/Script/Mini-jeux Poulet/ObstacleScript.cs
+++ Assets/Script/Mini-jeux Poulet/ObstacleScript.cs
@@ -6,16 +6,44 @@ public class ObstacleScript : MonoBehaviour
 {
     [SerializeField] private GameObject player;  // Prend l'objet du joueur
     [SerializeField] private GameObject chicken; // Prend l'objet du poulet
+    private bool isFollowing;
+    private bool isGoingBack;
+    [SerializeField] private int i = 0;
+    [SerializeField] private int refreshRate = 300;
+    [SerializeField] private Vector3 oldPosition;
+    [SerializeField] private float speed = 2f;
 
-    private void OnCollisionEnter2D(Collision2D collision) // Quand on entre dans une collision
+    private void Update()
     {
-        if (collision.gameObject.tag == "Player") // Si le collision est avec le joueur
+        isFollowing = chicken.GetComponent<ChickenScript>().isFollowingPlayer;
+        if (i >= refreshRate)
         {
-            Vector3 playerCoor = player.transform.position;                 // Coordonnee du joueur
-            Vector3 moov = new Vector3(playerCoor.x + 10.0f, 0.0f, 0.0f);   // Coordonnee du mouvement du poulet
-
-            chicken.GetComponent<SpriteRenderer>().enabled = true;   // Montre le rendu du poulet
-            chicken.GetComponent<BoxCollider2D>().enabled = true;    // Re-met les collisions avec le poulet
+            oldPosition = chicken.transform.position;
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+        if (isGoingBack)
+        {
+            if (Vector3.Distance(chicken.GetComponent<Transform>().position,oldPosition) > 0.5f)
+            {
+                chicken.GetComponent<Transform>().position = Vector3.Lerp(chicken.GetComponent<Transform>().position, oldPosition, Time.deltaTime * speed);
+            }
+            else
+            {
+                isGoingBack = false;
+            }
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision) // Quand on entre dans une collision
+    {
+        if (collision.gameObject.tag == "Player" && isFollowing) // Si le collision est avec le joueur
+        {
+            chicken.GetComponent<ChickenScript>().isFollowingPlayer = false;
+            isGoingBack = true;
+        }
+    }
+
 }
