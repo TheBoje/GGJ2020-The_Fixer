b5b09e17130c9471b8cdaa1eff703ed28e257ba2
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-02-02 11:11:03 +0100

    dialogues + dialogue holder

--- Assets/Script/Scene_2/Scene_2Holder.cs
+++ Assets/Script/Scene_2/Scene_2Holder.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Scene_2Holder : MonoBehaviour
 {
+
+    [SerializeField] private DialoguePlayer dP;
+
+    private int _state;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _state = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (_state)
+        {
+            case 0 :
+                dP.Read(0);
+
+
+
+            default:
+                break;
+        }
     }
 }
